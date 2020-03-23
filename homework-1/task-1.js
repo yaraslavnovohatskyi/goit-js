dfed7ecf7aa0da7c1c3f3ff57baf15af1ef5e8fc
yaraslavnovohatskyi <yaroslav.novohatskii@gmail.com>
2020-03-23 13:01:58 +0200

    task 1 is done

--- homework-1/task-1.js
+++ homework-1/task-1.js
@@ -1,1 +1,6 @@
 "use strict";
+const name = "генератор защитного поля";
+let price = 1000;
+console.log(`Вибран"${name}",цена за штуку ${price} кредитов`);
+price = 2000;
+console.log(`Вибран"${name}",цена за штуку ${price} кредитов`);
