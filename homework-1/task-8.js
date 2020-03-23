ba325e3130aa1fd6236825ef56abf701a22d42ad
yaraslavnovohatskyi <yaroslav.novohatskii@gmail.com>
2020-03-23 16:06:30 +0200

    task8 is done

--- homework-1/task-8.js
+++ homework-1/task-8.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let total = 0;
+let input = +prompt("Введите число");
+while (input) {
+ input = +prompt("ЕЩЕ ОДНО ЧИСЛО");
+  total += input;
+  console.log(total);
+}
+console.log("total: ", total);
