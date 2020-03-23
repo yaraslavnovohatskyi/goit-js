0881b48a7a867132f5f5f542d0ca0a658660e74f
yaraslavnovohatskyi <yaroslav.novohatskii@gmail.com>
2020-03-23 16:02:30 +0200

    task7 is done

--- homework-1/task-7.js
+++ homework-1/task-7.js
@@ -1,1 +1,35 @@
 "use strict";
+
+let country;
+country = prompt("Введите страну!").toLowerCase);
+
+let countryPay;
+switch () {
+case "китай":
+        countryPay = 180;
+alert()
+break;
+    
+case "чили":
+        countryPay = 250;
+alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+break; 
+        
+case "австралия":
+        countryPay = 170;
+alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+break; 
+        
+case "индия":
+        countryPay = 80;
+alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+break;
+
+case "ямайка":
+        countryPay = 120;
+alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+break;
+
+default:
+alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+}
