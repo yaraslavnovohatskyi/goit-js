ea53f05f788e213e46952ece887a321acbfe3c53
yaraslavnovohatskyi <yaroslav.novohatskii@gmail.com>
2020-03-23 14:50:49 +0200

    task4 is done

---
+++ homework-1/task-4.js
@@ -0,0 +1,21 @@
+"use strict";
+//№1
+const total = 100;
+const ordered = 150;
+if(ordered > total) {
+    console.log("На складе недостаточно товаров!");
+} else {
+    console.log("Заказ оформлен, с вами свяжется менеджер");
+}
+ordered = 100;
+if(ordered > total) {
+    console.log("На складе недостаточно товаров!");
+} else {
+    console.log("Заказ оформлен, с вами свяжется менеджер");
+}
+ordered = 60;
+if(ordered > total) {
+    console.log("На складе недостаточно товаров!");
+} else {
+    console.log("Заказ оформлен, с вами свяжется менеджер");
+}
